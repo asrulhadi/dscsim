83b096ade4f566277dcea2fddc6cecee522a89f7
dscsim <dscsim@2fbec121-4e48-0410-a7cc-a7fad1e65a2b>
2007-02-20 23:19:00 +0000

    controller logic changes.

--- trunk/src/main/java/net/sourceforge/dscsim/controller/MultiClu.java
+++ trunk/src/main/java/net/sourceforge/dscsim/controller/MultiClu.java
@@ -53,7 +53,11 @@ public class MultiClu  implements  BusListener, Constants {
 		public List getScreenCallStack() {
 			return _oScreenStack;
 		}
-		
+		/**
+		 * Return the index of the target screen.
+		 * @param oTarget.
+		 * @return index of screen on stack.
+		 */
 		private int indexOf(ScreenContent oTarget){
 			
 			if(oTarget == null) 
@@ -73,6 +77,25 @@ public class MultiClu  implements  BusListener, Constants {
 			return -1;
 		}
 		
+		/**
+		 * remove screen from the top of the stack if the screen is  
+		 * SendDistressScreen screen. It could be that the user has closed
+		 * the screen and that it's no long on top.
+		 * @param oMessage
+		 * @throws Exception
+		 */
+		private void popStockDistress(BusMessage oMessage) throws Exception{			
+			int idxLast = _oScreenStack.size() -1;			
+			if(idxLast>0){				
+				ScreenContent oScreen = (ScreenContent)_oScreenStack.remove(idxLast);				
+				if(oScreen instanceof SendDistressScreen){
+					oScreen.exit(oMessage);					
+					oScreen = (ScreenContent)_oScreenStack.get(idxLast-1);					
+					_oContext.getController().setScreenContent(oScreen);				
+				}				
+			}
+		}
+			
 		private void popStock(BusMessage oMessage) throws Exception{
 			
 			int idxLast = _oScreenStack.size() -1;
@@ -124,31 +147,35 @@ public class MultiClu  implements  BusListener, Constants {
 
 				try {			
 					
-					if(!_powerOn 	
-							&& !BusMessage.MSGTYPE_KEY.equals(oMessage.getType())){
+					/*if the power is off then ingnore all other type except keyboard.*/
+					if(!_powerOn && !BusMessage.MSGTYPE_KEY.equals(oMessage.getType())){
 						return;
 					}
 					
+					/*the source of the message is another client.*/
 					if(BusMessage.MSGTYPE_NETWORK.equals(oMessage.getType())){
 						
 						DscMessage oDscMessage = oMessage.getDscMessage();
+						String callType =oDscMessage.getCallType();
+						String toMMSI = oDscMessage.getToMMSI();
+						String myMMSI = _oContext.getApplicationContext().getIndividualMmsi();
 						
-						String strScreenName = _oContext.getContentManager().getCallTypeMappingValue(oDscMessage.getCallType());
-						
-						oScreen = _oContext.getContentManager().getScreenContent(strScreenName, _oContext);
-								
+						/*first remove distress screen from stack if it's a distress ack and is for me.*/
+						if(CALL_TYPE_DISTRESS_ACK.equals(callType)
+							&& myMMSI.equals(toMMSI)){							
+							popStockDistress(oMessage);							
+						}
+						/*now show incoming ack*/
+						String strScreenName = _oContext.getContentManager().getCallTypeMappingValue(oDscMessage.getCallType());						
+						oScreen = _oContext.getContentManager().getScreenContent(strScreenName, _oContext);								
 						oScreen.setIncomingDscMessage(oDscMessage);
 						
 						//copy the incoming to the outcoming just to have some defaults
 						DscMessage oCopy = new DscMessage(oDscMessage);
 						oScreen.setOutGoingDscMessage(oCopy);
-						
 						oScreen.enter(oCopy);
-
 						_oScreenStack.add(oScreen);
-						 		 
-						_oContext.getController().setScreenContent(oScreen);					
-						
+						_oContext.getController().setScreenContent(oScreen);
 						return;
 					}
 					
@@ -158,37 +185,29 @@ public class MultiClu  implements  BusListener, Constants {
 						resetScreenStack(oMessage);
 						return;
 					} 
-					
-					//get top of stack
-					
-					//SOS push start count down
+						
+					//SOS pushed. Start distress call process.
 					if(_powerOn 
 						&& BusMessage.MSGTYPE_KEY.equals(oMessage.getType())
 						&& FK_SOS.equals(oMessage.getButtonEvent().getKeyId())){
 						
-						oScreen = (ScreenContent)_oScreenStack.get(_oScreenStack.size()-1);
-						
+						oScreen = (ScreenContent)_oScreenStack.get(_oScreenStack.size()-1);						
 						if((oScreen instanceof SendDistressScreen)==false){
 							oScreen = _oContext.getContentManager().getScreenContent("distress_call_send", _oContext);
 							_oScreenStack.add(oScreen);
 							_oContext.getController().setScreenContent(oScreen);								
-						} else {
-							
+						} else {							
 							if(oScreen.signal(oMessage) == null)
-								resetScreenStack(oMessage);
-							
+								resetScreenStack(oMessage);							
 						}
-
 						return;
 					}
 					
 				
 
 					//not handled default to screen actions
-					oScreen = (ScreenContent)_oScreenStack.get(_oScreenStack.size()-1);
-					
+					oScreen = (ScreenContent)_oScreenStack.get(_oScreenStack.size()-1);			
 					ScreenContent oReturnScreen = oScreen.signal(oMessage);
-					
 					if(oReturnScreen == null){
 						//send clr message and return
 						//Bus.getInstance().publish(new BusMessage(null, FK_CLR));
@@ -196,49 +215,39 @@ public class MultiClu  implements  BusListener, Constants {
 						popStock(oMessage);
 						return;
 						
-					} else if(oReturnScreen != oScreen){
-						
-						
+					} else if(oReturnScreen != oScreen){						
 						int idxOf = indexOf(oReturnScreen);
-						
 						//instance of screen is already on stack backup to it.
 						//therefore backup stack. no cylces in state diagram.
-						if(idxOf > -1){
-						
+						if(idxOf > -1){						
 							oScreen.exit(oMessage);
-
 							ScreenContent oTmp=null;
 							for(int i=_oScreenStack.size()-1; i >= idxOf; i--){
 								oTmp = (ScreenContent)_oScreenStack.get(i);
 								//oTmp.exit(oMessage);
 								_oScreenStack.remove(i);
 							}
-						
 						} else {
 							//moving down deeper in state diagram
 							oScreen.exit(oMessage);
-							
 							oReturnScreen.setIncomingDscMessage(oScreen.getIncomingDscMessage());
 							oReturnScreen.setOutGoingDscMessage(oScreen.getOutGoingDscMessage());
 						}
 											
 						oReturnScreen.enter(oScreen.getIncomingDscMessage());
-						
 						_oScreenStack.add(oReturnScreen);
 					}
 					
 					_oContext.getController().setScreenContent(oReturnScreen);				
 					
 					if(BusMessage.MSGTYPE_KEY.equals(oMessage.getType())
-							&& oMessage.getButtonEvent().isMasterSwitch()){
-						
+							&& oMessage.getButtonEvent().isMasterSwitch()){						
 						String keyId = oMessage.getButtonEvent().getKeyId();		
 						if(DSC_POWERED_OFF.equals(keyId)){
 							_powerOn = false;
 						}else if(DSC_POWERED_ON.equals(keyId)){
 							_powerOn = true;
-						}
-						
+						}				
 					}
 							
 				}catch(Exception oEx){
