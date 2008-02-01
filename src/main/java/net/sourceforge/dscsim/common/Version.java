d14215ab8361ce48972539af312dbf36ccf8bbcf
ohecker <ohecker@2fbec121-4e48-0410-a7cc-a7fad1e65a2b>
2008-02-01 19:41:26 +0000

    changed version info to 1.3.3-SNAPSHOT

--- src/main/java/net/sourceforge/dscsim/common/Version.java
+++ src/main/java/net/sourceforge/dscsim/common/Version.java
@@ -28,7 +28,7 @@ public class Version {
 	/**
 	 * The constant containing the version information
 	 */
-	private static final String VERSION = "1-SNAPSHOT";
+	private static final String VERSION = "1.3.3-SNAPSHOT";
 
 	/**
 	 * Returns version information about the application
