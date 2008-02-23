f26948a62e42282cb79185f1151f817c0ccb10e1
dscsim <dscsim@2fbec121-4e48-0410-a7cc-a7fad1e65a2b>
2008-02-23 21:33:53 +0000

    remove hibernate annotation dependency.

--- src/main/java/net/sourceforge/dscsim/controller/persistence/HibernateUtil.java
+++ src/main/java/net/sourceforge/dscsim/controller/persistence/HibernateUtil.java
@@ -17,7 +17,7 @@ public class HibernateUtil {
 	static {
 		try {
 			// Create the SessionFactory from hibernate.cfg.xml
-			sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+			sessionFactory = new Configuration().configure().buildSessionFactory();
 		} catch (Throwable ex) {
 			// Make sure you log the exception, as it might be swallowed
 			System.err.println("Initial SessionFactory creation failed." + ex);
